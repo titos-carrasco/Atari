7161eb26072b1da270c0c0be068ae8deea0e87a2
Roberto Carrasco <titos.carrasco@gmail.com>
2025-11-07 23:14:05 -0300

    Agrega código para leer desde la serial y enviarlo a la salida midi

--- MidiBridge/MidiBridge.py
+++ MidiBridge/MidiBridge.py
@@ -1,27 +1,49 @@
-import time
-import socket
+import serial
+import rtmidi
 
 
 class MidiBridge:
     def __init__(self):
         self.conn = None
 
-        # -- la consola remota (Modem Atari)
-        self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.conn.settimeout(1)
-        self.conn.connect(("127.0.0.1", 9000))
+        # midi port
+        self.midi_out = rtmidi.MidiOut()
+        self.midi_out.open_virtual_port("Atari Bridge Port")
+
+        # Atari SIO
+        self.conn = serial.Serial("/dev/ttyUSB0", baudrate=31250, timeout=1)
 
     def run(self):
         while True:
             try:
-                data = self.conn.recv(1)
-                ch = data.decode("iso8859-1")
+                b0 = self.conn.read(1)
+                if b0 == b"":
+                    continue
+                b0 = b0[0]
 
-                if ch == "\r" or ch == "\n":
-                    print(flush=True)
+                # system common messages
+                if b0 & 0xF0 == 0xF0:
+                    if b0 == 0xF0:
+                        pass
+                    elif b0 in (0xF1, 0xF3):
+                        b1 = self.conn.read(1)[0]
+                        self.midi_out.send_message([b0, b1])
+                    elif b0 == 0xF2:
+                        b1 = self.conn.read(1)[0]
+                        b2 = self.conn.read(1)[0]
+                        self.midi_out.send_message([b0, b1, b2])
+                    else:
+                        self.midi_out.send_message([b0])
+
+                # channel voice/mode messages
+                elif b0 & 0xF0 in (0xC0, 0xD0):
+                    b1 = self.conn.read(1)[0]
+                    self.midi_out.send_message([b0, b1])
                 else:
-                    print(data, end="", flush=True)
-            except socket.timeout:
+                    b1 = self.conn.read(1)[0]
+                    b2 = self.conn.read(1)[0]
+                    self.midi_out.send_message([b0, b1, b2])
+            except serial.SerialTimeoutException:
                 pass
             except KeyboardInterrupt:
                 break
@@ -29,9 +51,5 @@ class MidiBridge:
 
 
 # --- show time
-# probado con Altirra
-#   - SIO serial adapter - Networked serial port - port 9000/tcp at 19200
-#   - xxxxx
-
 app = MidiBridge()
 app.run()
